d66443d3f4cda588e9989c9e76b6941d432ae1b5
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-10 23:14:08 -0300

    Add files via upload

---
+++ tres_valores_numericos.py
@@ -0,0 +1,10 @@
+
+n_numero1 = float(input("Digite um numero: ").replace(",", "."))
+n_numero2 = float(input("Digite o numero: ").replace(",", "."))
+n_numero3 = float(input("Digite o terceiro numero: ").replace(",", "."))
+
+soma = n_numero1 + n_numero2 + n_numero3
+print(f"A soma dos numeros é: {soma}")
+
+
+
