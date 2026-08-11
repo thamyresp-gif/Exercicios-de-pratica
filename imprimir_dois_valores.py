9d2bc5d0ae94952b5b5366b77be66e175ccc4dc4
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-10 23:07:58 -0300

    Add files via upload

---
+++ imprimir_dois_valores.py
@@ -0,0 +1,7 @@
+
+
+n_numero1 = float(input("digite um numero: ").replace(",", "."))
+n_numero2 = float(input("digite o numero: ").replace(",", "."))
+
+soma = n_numero1 + n_numero2
+print(f"A soma dos numeros é: {soma}")
---
+++ nome idade altura.py
@@ -0,0 +1,8 @@
+
+print ("Olá, Bem vindo!")
+
+nome_usuario =  str(input("Digite seu nome completo: ")) 
+idade_usuario = int(input("Digite sua idade: "))
+altura_usuario = float(input("Digite sua altura em metros: "))
+
+print("Nome do usuario é %s, idade é %d e altura é %.2f metros." % (nome_usuario, idade_usuario, altura_usuario))
