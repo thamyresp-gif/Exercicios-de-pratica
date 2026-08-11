11c6c3a519409d87c70a1fe2c2a031e0e08d298e
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-10 22:22:11 -0300

    Add files via upload

---
+++ nome idade.py
@@ -0,0 +1,8 @@
+
+print("Olá, Bem vindo!")
+
+nome_usuario = str(input("Digite seu nome completo: "))
+
+idade_usuario = int(input("Digite sua idade: "))
+
+print(f"Seu nome é {nome_usuario} e você tem {idade_usuario} anos de idade.")
