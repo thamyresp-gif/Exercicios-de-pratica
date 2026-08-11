8eb3a883fbf82f0659674ded016b4cc03b16c167
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-10 23:29:02 -0300

    Add files via upload

---
+++ multiplicacao.py
@@ -0,0 +1,8 @@
+
+n_numero1 = float((input("Digite um numero:")).replace(",", "."))
+n_numero2 = float((input("Digite o segundo numero:")).replace(",", "."))
+
+multiplicacao = n_numero1 * n_numero2
+print(f"A multiplicação dos dois numeros é: {multiplicacao}")
+
+
