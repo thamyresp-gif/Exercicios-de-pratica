837a6f6b5d220c7e208f052314001e661834b27f
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-10 23:35:53 -0300

    Add files via upload

---
+++ denominador.py
@@ -0,0 +1,11 @@
+
+#Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e realize a divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
+
+n_numero1 = float (input("Digite um numero: ").replace(",", ".")) 
+n_numero2 = float(input("Digite o denominador: ").replace(",", "."))
+
+if n_numero2 == 0:
+    print("Erro: o denominador não pode ser 0.")
+else:
+    divisao = n_numero1 / n_numero2
+    print(f"A divisão entre {n_numero1} e {n_numero2} é: {divisao}")
