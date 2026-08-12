eb7d3f8ad97941d65ea9dcda8be73cb031a4c043
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-11 23:57:20 -0300

    Add files via upload

---
+++ media_ponderada.py
@@ -0,0 +1,21 @@
+
+#Crie um código que calcule e imprima a média ponderada dos números 5, 12, 20 e 15 com pesos respectivamente iguais a 1, 2, 3 e 4.
+
+nota1 = 5
+peso1 = 1
+
+nota2 = 12
+peso2 = 2
+
+nota3 = 20
+peso3 = 3
+
+nota4 = 15
+peso4 = 4
+
+
+media_ponderada = (nota1*peso1 + nota2*peso2 + nota3*peso3 + nota4*peso4) / (peso1 + peso2 + peso3 + peso4)
+
+print(f"A media ponderada é {media_ponderada}")
+
+
