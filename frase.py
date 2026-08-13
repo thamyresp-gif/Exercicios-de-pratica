f368a40b1ba5272daa0ae44b9fe8ca0b8b1ed9b2
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-12 22:52:05 -0300

    Add files via upload

---
+++ frase.py
@@ -0,0 +1,8 @@
+
+#Crie uma variável chamada “frase” e atribua a ela uma string de sua escolha. Em seguida, imprima a frase na tela.
+
+import string
+
+frase_f = input("Digite uma frase: ")
+
+print(frase_f)
