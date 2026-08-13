b1779abe4fed3e2019a97d4da7c07e0d71e6117c
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-12 23:22:18 -0300

    Add files via upload

---
+++ frase2.py
@@ -0,0 +1,7 @@
+
+#Crie um código que solicite uma frase e depois imprima a frase na tela.
+
+
+texto_1 = input("Digite uma frase: ")
+
+print(texto_1)
---
+++ frase4.py
@@ -0,0 +1,6 @@
+
+#Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase digitada mas com todas as letras minúsculas.
+
+frase = input("Digite uma frase: ").lower()
+
+print(frase)
