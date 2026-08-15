b067ab79934636e255dddb88bd014bbe10594d2f
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-14 22:49:14 -0300

    Add files via upload

---
+++ frase9.py
@@ -0,0 +1,6 @@
+
+#Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “a” trocadas pela caractere “@”.
+
+frase = input("Digite a frase: ")
+
+print(frase.replace("a","@"))
