d8e48426b58fa85d025ec19053e3e2155806917a
Thamyres Machado <thamyres_p@hotmail.com>
2026-08-14 22:55:35 -0300

    Add files via upload

---
+++ frase10.py
@@ -0,0 +1,9 @@
+
+#Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as consoantes “s” trocadas pelo caractere “$”
+
+frase = input("Digite uma frase: ")
+
+print (frase.replace("s",chr(36)))
+
+
+
